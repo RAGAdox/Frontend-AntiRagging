66f9d5ee39e815cde42adefb43912aa1c52146a9
RAGAdox <rishitech882@gmail.com>
2019-03-25 01:56:55 +0530

    Login using JWT with UI made

--- app/src/main/java/com/example/antiragingappkgec/MainActivity.java
+++ app/src/main/java/com/example/antiragingappkgec/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -23,31 +25,62 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     Auth authenticator=new Auth();
-    final static String url="http://192.168.42.185:2000";//IP address of the Backend server Connected to a P2P enabled Network and accessible from the development test Device
+    EditText username,password;
+    final static String url="http://192.168.42.97:2000";//IP address of the Backend server Connected to a P2P enabled Network and accessible from the development test Device
+    RequestQueue requestQueue;
+    JsonObjectRequest getProfile;
+    public JsonObjectRequest postLoginf(String usernameStr,String passwordStr){
+        JsonObjectRequest postLogin=new JsonObjectRequest(
+            Request.Method.POST,
+            url + "/passauth/login?username="+usernameStr+"&password="+passwordStr,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+
+                    try {
+                        authenticator.token=response.getString("token");
+                        Log.e("Rest API Login Token", response.getString("token"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                Log.e("RestAPI Login Error",error.toString());
+            }
+        });
+        return postLogin;
+    }
+    JsonObjectRequest getHome=new JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            (String) null,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    Log.e("Rest API Response",response.toString());
+                }
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("Rest API Error Response",error.toString());
+                }
+            }
+    );
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.LoginBtn);
+        username=(EditText)findViewById(R.id.username);
+        password=(EditText)findViewById(R.id.password);
+        //Button button = (Button) findViewById(R.id.LoginBtn);
+        requestQueue= Volley.newRequestQueue(this);
 
-        RequestQueue requestQueue= Volley.newRequestQueue(this);
-        JsonObjectRequest getHome=new JsonObjectRequest(
-                Request.Method.GET,
-                url,
-                (String) null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.e("Rest API Response",response.toString());
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("Rest API Error Response",error.toString());
-                    }
-                }
-        );
+
+        /*
         //Dummy Protected Route Access Using JWT token
         JsonObjectRequest getProfile=new JsonObjectRequest(
                 Request.Method.GET,
@@ -75,33 +108,18 @@ public class MainActivity extends AppCompatActivity {
                 return headers;
             }
         };
-
+        */
         //Dummy login for getting access Token from JWT
-        JsonObjectRequest postLogin=new JsonObjectRequest(
-                Request.Method.POST,
-                url + "/passauth/login?username=admin&password=admin",
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            authenticator.token=response.getString("token");
-                            Log.e("Rest API Login Token", response.getString("token"));
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("RestAPI Login Error",error.toString());
-                    }
-                }
-        );
+        /*
         requestQueue.add(getHome);
         requestQueue.add(postLogin);
         requestQueue.add(getProfile);//Sends false as Token is not ready yet and all actions takes place in async
+        */
 
     }
-
+    public void onClickLogin(View view)
+    {
+        Log.e("Credentials",username.getText()+"&&"+password.getText());
+        requestQueue.add(this.postLoginf(username.getText().toString(),password.getText().toString()));
+    }
 }
